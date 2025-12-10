7c089746ecb1e91b64edf1ea383a2da9942b962e
FerretCode <eggertet@oregonstate.edu>
2025-12-09 23:34:19 -0800

    add darwin

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -e
 
 GOOS=linux GOARCH=amd64 go build -o kitbuilder-linux-amd64
 GOOS=windows GOARCH=amd64 go build -o kitbuilder-windows-amd64.exe
+GOOS=darwin GOARCH=amd64 go build -o kitbuilder-darwin-amd64
